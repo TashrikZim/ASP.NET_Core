9acbb0af8c4415c54b97a03446f25a028acb3080
Tashrik Zim <tanjiltashrikzim@gmail.com>
2026-02-24 00:21:45 +0600

    partial work of task 1

---
+++ StudentProfile/StudentProfile/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentProfile.Models;
+
+namespace StudentProfile.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    public IActionResult Profile(int id)
+        {
+            Student student = new Student()
+            {
+                Id = id,
+                Name = "Tanjil Tashrik ",
+                Age = 24,
+                Department = "CS",
+                Cgpa = "3"
+            };
+
+            return View();
+        }
+
+    }
+}
---
+++ StudentProfile/StudentProfile/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentProfile.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+        public string Cgpa { get; set; }
+    }
+}
