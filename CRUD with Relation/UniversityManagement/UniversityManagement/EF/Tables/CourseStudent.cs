6cb9f9acd788f4c508a471999bfaef7feae45518
Tashrik Zim <tanjiltashrikzim@gmail.com>
2026-04-14 02:00:57 +0600

    Tables with relationship

---
+++ CRUD with Relation/UniversityManagement/UniversityManagement/EF/Tables/CourseStudent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniversityManagement.EF.Tables;
+
+public partial class CourseStudent
+{
+    public int Id { get; set; }
+
+    public int StudentId { get; set; }
+
+    public int CourseId { get; set; }
+
+    public string Student { get; set; } = null!;
+
+    public string Course { get; set; } = null!;
+
+    public virtual Course CourseNavigation { get; set; } = null!;
+
+    public virtual Student StudentNavigation { get; set; } = null!;
+}
