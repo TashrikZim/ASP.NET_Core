5e24443b75bcc9c2700e540ad17758a40b939626
Tashrik Zim <tanjiltashrikzim@gmail.com>
2026-04-21 15:16:59 +0600

    db inject

--- Login and Registration/Auth/Auth/Program.cs
+++ Login and Registration/Auth/Auth/Program.cs
@@ -1,7 +1,14 @@
+using Auth.EF;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AuthRegistrationContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConn"));
+});
 
 var app = builder.Build();
 
--- Login and Registration/Auth/Auth/obj/Debug/net10.0/Auth.AssemblyInfo.cs
+++ Login and Registration/Auth/Auth/obj/Debug/net10.0/Auth.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cb9f9acd788f4c508a471999bfaef7feae45518")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c51f71e188ae9da729e2ae839a2c895a12a77db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
