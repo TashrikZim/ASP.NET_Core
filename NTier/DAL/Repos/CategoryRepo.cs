9f4fed876d2ca067be8a70516a820cdc25b60baf
Tashrik Zim <tanjiltashrikzim@gmail.com>
2026-05-10 15:53:44 +0600

    Three tier

---
+++ NTier/DAL/Repos/CategoryRepo.cs
@@ -0,0 +1,44 @@
+﻿using DAL.EF;
+using DAL.EF.Tables;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repos
+{
+    public class CategoryRepo
+    {
+        NLayerContext db_data;
+        public CategoryRepo(NLayerContext db_data)
+        {
+            this.db_data = db_data;
+        }
+        public bool Create(Category C)
+        {
+            db_data.Categories.Add(C);
+            return db_data.SaveChanges() > 0;
+        }
+        public List<Category> Get()
+        {
+            return db_data.Categories.ToList();
+        }
+        public Category Get(int id) {
+
+            return db_data.Categories.Find(id);
+        }
+        public bool Update(Category C)
+        {
+            var exobj = Get(C.Id);
+            db_data.Entry(exobj).CurrentValues.SetValues(C);
+            return db_data.SaveChanges() > 0;
+        }
+        public bool Delete(int id) {
+            var exobj = Get(id);
+            db_data.Remove(exobj);
+            return db_data.SaveChanges() > 0; 
+        
+        }
+
+
+    }
+}
---
+++ NTier/DAL/Repos/ProductRepo.cs
@@ -0,0 +1,47 @@
+﻿using DAL.EF;
+using DAL.EF.Tables;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repos
+{
+    public class ProductRepo
+    {
+
+        NLayerContext db_data;
+        public ProductRepo(NLayerContext db_data)
+        {
+            this.db_data = db_data;
+        }
+        public bool Create(Product C)
+        {
+            db_data.Products.Add(C);
+            return db_data.SaveChanges() > 0;
+        }
+        public List<Product> Get()
+        {
+            return db_data.Products.ToList();
+        }
+        public Product Get(int id)
+        {
+
+            return db_data.Products.Find(id);
+        }
+        public bool Update(Product C)
+        {
+            var exobj = Get(C.Id);
+            db_data.Entry(exobj).CurrentValues.SetValues(C);
+            return db_data.SaveChanges() > 0;
+        }
+        public bool Delete(int id)
+        {
+            var exobj = Get(id);
+            db_data.Remove(exobj);
+            return db_data.SaveChanges() > 0;
+
+        }
+
+
+    }
+}
